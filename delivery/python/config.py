ae95f47ea8f0b54c2aed33118973395033eb965d
permafrost-aleung <anthony@permafrostai.com>
2026-08-19 08:43:42 -0400

    delivery files init

---
+++ delivery/python/config.py
@@ -0,0 +1,11 @@
+import os
+import json
+import sys
+
+# Snowflake connection config
+ACCOUNT   = "YOUR_ACCOUNT"
+USER      = "YOUR_USER"
+WAREHOUSE = "YOUR_WAREHOUSE"
+DATABASE  = "YOUR_DATABASE"
+SCHEMA    = "YOUR_SCHEMA"
+ROLE      = "YOUR_ROLE"
